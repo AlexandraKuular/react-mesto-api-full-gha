29fb2cafe6cc6b5ba852f86f36e8c310242124ed
Alexandra Kuular <sakuular@gmail.com>
2023-08-20 14:53:01 +0300

    ПР15

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
