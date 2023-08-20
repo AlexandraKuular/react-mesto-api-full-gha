6f07cd1e9980412725fbc4a31eb2473f7df68caf
Alexandra Kuular <sakuular@gmail.com>
2023-08-20 21:24:51 +0300

    ПР15

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,7 +11,8 @@ const cors = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    console.log(origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
